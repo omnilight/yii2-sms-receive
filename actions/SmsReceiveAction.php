a6e6d649de68af40ecbb26e822e529d015c2b12e
Pavel <pavel.agalecky@gmail.com>
2015-11-06 11:57:48 +0300

    Updated SmsReceiveAction

--- actions/SmsReceiveAction.php
+++ actions/SmsReceiveAction.php
@@ -3,6 +3,8 @@
 namespace omnilight\sms\receive\actions;
 use omnilight\sms\receive\contracts\ProviderInterface;
 use yii\base\Action;
+use yii\base\Component;
+use yii\base\Event;
 
 
 /**
@@ -10,12 +12,41 @@ use yii\base\Action;
  */
 class SmsReceiveAction extends Action
 {
+    /**
+     * @var array | string
+     */
     public $provider;
+    /**
+     * @var Callable
+     */
+    public $receiveSms;
+    /**
+     * @var Callable
+     */
+    public $error;
 
     public function run()
     {
-        /** @var ProviderInterface $provider */
+        /** @var ProviderInterface | Component $provider */
         $provider = \Yii::createObject($this->provider);
+        $this->attachHandlers($provider);
         return $provider->receive(\Yii::$app->request)->answer();
+    }
+
+    /**
+     * @param ProviderInterface $provider
+     */
+    private function attachHandlers(ProviderInterface $provider)
+    {
+        if (!($provider instanceof Component)) {
+            return;
+        }
+        /** @var Component $provider */
+        if ($this->receiveSms !== null) {
+            $provider->on(ProviderInterface::EVENT_RECEIVE_SMS, $this->receiveSms);
+        }
+        if ($this->error !== null) {
+            $provider->on(ProviderInterface::EVENT_ERROR, $this->error);
+        }
     }
 }
